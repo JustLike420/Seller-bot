45fafc23b513b7dcf72ecbc5a572534cb87a16ce
Knyz_ <justlikevova@gmail.com>
2022-04-19 23:06:53 +0300

    fix hebrew text in email

--- utils/send_email.py
+++ utils/send_email.py
@@ -1,4 +1,6 @@
 import smtplib
+from email.mime.text import MIMEText
+
 from data.config import email_login, email_password, recipient
 
 
@@ -13,8 +15,9 @@ async def send_email(message):
     try:
         server.login(sender, password)
         # recipient = 'justlikevova@gmail.com'
-        server.sendmail(sender, recipient, f"Subject: {title}\n{message}")
-        print('send')
+        text = MIMEText(message, 'plain')
+        server.sendmail(sender, recipient, f"Subject: {title}\n{text}")
+        print('sent')
         return "Message was send"
     except Exception as e:
         return f'{e}\nCheck your login or password'
